1712a18b128946893f97f9b2ecd4eaf5fa9bded3
Michael Bisbjerg <michael@mbwarez.dk>
2015-06-13 20:54:03 +0200

    Basic error handling

--- SslLabsLib/SslLabsClient.cs
+++ SslLabsLib/SslLabsClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net;
+using System.Threading.Tasks;
 using RestSharp;
+using RestSharp.Extensions;
 using SslLabsLib.Enums;
 using SslLabsLib.Objects;
 
@@ -50,8 +52,56 @@ namespace SslLabsLib
 
         public Analysis GetAnalysis(string hostname, int? maxAge = null, AnalyzeOptions options = AnalyzeOptions.None)
         {
+            Analysis analysis;
+            AnalysisResult result = GetAnalysisInternal(hostname, maxAge, options, out analysis);
+
+            switch (result)
+            {
+                case AnalysisResult.Success:
+                    return analysis;
+                case AnalysisResult.Error:
+                case AnalysisResult.UnknownError:
+                    throw new Exception("The server was unable to handle the request (" + result + ")");
+                case AnalysisResult.Maintenance:
+                    throw new Exception("The server was unable to handle the request due to Maintenance (HTTP 503)");
+                case AnalysisResult.RateLimit:
+                    throw new Exception("The server was unable to handle the request due to rate limits (HTTP 429)");
+                case AnalysisResult.Overloaded:
+                    throw new Exception("The server was unable to handle the request due to being overloaded (HTTP 529)");
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public Endpoint GetCachedEndpointAnalysis(string hostname, IPAddress endpoint)
+        {
+            IRestRequest req = new RestRequest("getEndpointData")
+                .AddQueryParameter("host", hostname)
+                .AddQueryParameter("s", endpoint.ToString());
+
+            IRestResponse<Endpoint> resp = _restClient.Execute<Endpoint>(req);
+
+            ReadXHeaders(resp);
+
+            return resp.Data;
+        }
+
+        private void ReadXHeaders<T>(IRestResponse<T> resp)
+        {
+            foreach (Parameter header in resp.Headers)
+            {
+                int tmp;
+                if (header.Name == "X-Max-Assessments" && int.TryParse(header.Value as string, out tmp))
+                    MaxAssesments = tmp;
+                else if (header.Name == "X-Current-Assessments" && int.TryParse(header.Value as string, out tmp))
+                    CurrentAssesments = tmp;
+            }
+        }
+
+        private AnalysisResult GetAnalysisInternal(string hostname, int? maxAge, AnalyzeOptions options, out Analysis analysis)
+        {
             IRestRequest req = new RestRequest("analyze")
-                                .AddQueryParameter("host", hostname);
+                      .AddQueryParameter("host", hostname);
 
             if (options.HasFlag(AnalyzeOptions.Publish))
                 req.AddQueryParameter("publish", "on");
@@ -73,32 +123,41 @@ namespace SslLabsLib
 
             ReadXHeaders(resp);
 
-            return resp.Data;
+            analysis = null;
+            if (resp.StatusCode == HttpStatusCode.BadRequest || resp.StatusCode == HttpStatusCode.InternalServerError)
+                // 400 - invocation error (e.g., invalid parameters)
+                // 500 - internal error
+                return AnalysisResult.Error;
+
+            if (resp.StatusCode == (HttpStatusCode)429)
+                // 429 - client request rate too high
+                return AnalysisResult.RateLimit;
+
+            if (resp.StatusCode == HttpStatusCode.ServiceUnavailable)
+                // 503 - the service is not available (e.g., down for maintenance)
+                return AnalysisResult.Maintenance;
+
+            if (resp.StatusCode == (HttpStatusCode)529)
+                // 529 - the service is overloaded
+                return AnalysisResult.Overloaded;
+
+            if (resp.StatusCode != HttpStatusCode.OK)
+                return AnalysisResult.UnknownError;
+
+            // Success
+            analysis = resp.Data;
+
+            return AnalysisResult.Success;
         }
 
-        public Endpoint GetCachedEndpointAnalysis(string hostname, IPAddress endpoint)
+        enum AnalysisResult
         {
-            IRestRequest req = new RestRequest("getEndpointData")
-                                .AddQueryParameter("host", hostname)
-                                .AddQueryParameter("s", endpoint.ToString());
-
-            IRestResponse<Endpoint> resp = _restClient.Execute<Endpoint>(req);
-
-            ReadXHeaders(resp);
-
-            return resp.Data;
-        }
-
-        private void ReadXHeaders<T>(IRestResponse<T> resp)
-        {
-            foreach (Parameter header in resp.Headers)
-            {
-                int tmp;
-                if (header.Name == "X-Max-Assessments" && int.TryParse(header.Value as string, out tmp))
-                    MaxAssesments = tmp;
-                else if (header.Name == "X-Current-Assessments" && int.TryParse(header.Value as string, out tmp))
-                    CurrentAssesments = tmp;
-            }
+            Success,
+            Error,
+            UnknownError,
+            Maintenance,
+            RateLimit,
+            Overloaded
         }
     }
 }
--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -11,9 +11,9 @@ namespace TestApplication
         static void Main(string[] args)
         {
             SslLabsClient client = new SslLabsClient();
-
+            
             Analysis analysis = client.GetAnalysis("csis.dk", null, AnalyzeOptions.FromCache | AnalyzeOptions.ReturnAllWhenDone);
-            var res = client.GetCachedEndpointAnalysis("csis.dk", IPAddress.Parse("46.51.179.119"));
+            var res =  client.GetCachedEndpointAnalysis("csis.dk", IPAddress.Parse("46.51.179.119"));
 
             var xa = res.Details.Cert;
 
