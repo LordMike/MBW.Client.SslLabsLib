b89cafb88f0ef8ca53f414c13551a9c6d9be587f
Michael Bisbjerg <mike@csis.dk>
2015-07-30 14:19:30 +0200

    Remove unused libraries (since removing preliminary checks)

--- SslLabsMassScan/Program.cs
+++ SslLabsMassScan/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Security;
-using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using Mono.Options;
