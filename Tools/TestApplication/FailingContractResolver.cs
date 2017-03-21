c19d11d3297600941ef58e8b911f0359ca499f7c
Michael Bisbjerg <michael@mbwarez.dk>
2017-03-21 22:15:30 +0100

    Make a JSON validation strategy in TestApplication

---
+++ Tools/TestApplication/FailingContractResolver.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace TestApplication
+{
+    public class FailingContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty res = base.CreateProperty(member, memberSerialization);
+
+            if (!res.Ignored)
+                // If we haven't explicitly stated that a field is not needed, we require it for compliance
+                res.Required = Required.AllowNull;
+
+            return res;
+        }
+    }
+}
--- Tools/TestApplication/Program.cs
+++ Tools/TestApplication/Program.cs
@@ -1,22 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 using SslLabsLib;
 using SslLabsLib.Enums;
-using SslLabsLib.Objects;
+using ErrorEventArgs = Newtonsoft.Json.Serialization.ErrorEventArgs;
 
 namespace TestApplication
 {
     class Program
     {
+        private static readonly List<ErrorEventArgs> Errors = new List<ErrorEventArgs>();
+
         static void Main(string[] args)
         {
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                MissingMemberHandling = MissingMemberHandling.Error,
+                ContractResolver = new FailingContractResolver(),
+                Error = Error
+            };
+
             SslLabsClient client = new SslLabsClient();
 
-            Host hostResult = client.GetAnalysisBlocking("mbwarez.dk", null, AnalyzeOptions.StartNew, analysis1 =>
-            {
-                Console.WriteLine("Status: " + analysis1.Status + " (" + analysis1.StatusMessage + ")");
-            });
+            // Run a method call
 
-            Console.WriteLine(hostResult);
+            //client.GetInfo();
+            client.GetAnalysis("ssllabs.com", null, AnalyzeOptions.FromCache | AnalyzeOptions.ReturnAll);
+            
+
+
+            // Examine JSON errors
+            Console.WriteLine($"There were {Errors.Count} JSON errors");
+
+            foreach (ErrorEventArgs error in Errors)
+            {
+                Console.WriteLine($"Message : {error.ErrorContext.Error.Message}");
+                Console.WriteLine($"  Path  : {error.ErrorContext.Path}");
+                Console.WriteLine($"  Member: {error.ErrorContext.Member}");
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void Error(object sender, ErrorEventArgs errorEventArgs)
+        {
+            Errors.Add(errorEventArgs);
+            errorEventArgs.ErrorContext.Handled = true;
         }
     }
 }
